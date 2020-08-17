b4fabaf18c555540f1582623eb0baf2fd76dea4d
Lucas Di Lorenzo <lucaspincha@outlook.com>
2020-08-17 14:18:42 -0300

    ejer4

--- Practica 4.py
+++ Practica 4.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from pattern.es import conjugate
 from pattern.es import INFINITIVE
 from pattern.es import parse,split
+import random
 
 
 def ejer1():
@@ -168,6 +169,59 @@ def ejer3():
         json.dump(dic,archivoFinal,indent=4)
  
 
+def ejer4():
+    """
+    ENUNCIADO:
+    En base al ejercicio 5 de la Práctica 3, diagramar una interfaz en 
+    PySimpleGUI que permita seleccionar dos archivos (utilizando el widget
+    FileBrowse). Uno de ellos contendrá colores y el otro coordenadas. Ambos
+    archivos se encuentran en texto plano, donde cada elemento se ubica en una
+    línea. Utilizando el widget Graph, dibuje en cada coordenada, un punto
+    (con DrawPoint) del color asociado (correspondiente a la misma línea de 
+    ambos archivos). Para realizar las pruebas, genere ambos archivos. Para
+    generar el de colores, investigue los disponibles en el ejemplo de colores
+    en Github.
+    """
+    layout = [[sg.Text('Seleccione los archivos de Txt')],
+              [sg.Text('Source for colores', size=(15, 1)), sg.InputText(), sg.FileBrowse(key='COLORES')],
+              [sg.Text('Source for coordenadas', size=(15, 1)), sg.InputText(), sg.FileBrowse(key='COORDENADAS')],
+              [sg.Button('Cargar'), sg.Cancel()],     
+              [sg.Graph(canvas_size=(200,200), graph_bottom_left=(0, 0), graph_top_right=(150, 150), background_color='white', enable_events=True, key='graph')]
+             ]
+    window = sg.Window('Ejer4',layout)
+    while True:
+        event, values = window.read()
+        if event is 'Cancel' or event is None:
+            break
+        if event is 'Cargar':   
+            try:
+                c = open(values[0],'r')
+                x = open(values[1],'r')
+                colores = c.read().split()
+                coordenadas = x.read().split()
+                lista=[]
+                # este if es para obteber la lista de menos elementos para 
+                # iterar sobre esa lista, sino en caso de que iteremos sobre
+                # la lista con mas elementos, al querer juntar ambos datos es
+                # posible que de error por encontrar un inice fuera de rango al
+                # recorrer la lista de menor tamaño
+                if len(colores) > len(coordenadas):
+                    cantidad_a_recorrer = len(coordenadas)
+                else:
+                    cantidad_a_recorrer = len(colores)   
+                for i in range(cantidad_a_recorrer):
+                    tupla = (colores[i],coordenadas[i])
+                    lista.append(tupla)
+                for i in lista: 
+                    print(i)
+                    coords=i[1].split(',')
+                    window['graph'].draw_point((int(coords[0]),int(coords[1])), 30, color=i[0])
+            except FileNotFoundError:
+                sg.popup('No se cargaron los archivos')
+
+ 
+    
+
 '''
     Ejer 1 Practica 4
 '''
@@ -182,4 +236,7 @@ def ejer3():
     Ejer 3 Practica 4
 '''
 #ejer3()
-
+'''
+    Ejer 4 Practica 4
+'''
+ejer4()
